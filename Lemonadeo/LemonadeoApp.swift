89e2b2c38d658a26b572eb958dcf37cb2beaca52
Elif Oskanbaş <elifoskanbas@gmail.com>
2025-04-15 13:13:47 +0300

    Initial Commit

---
+++ Lemonadeo/LemonadeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonadeoApp.swift
+//  Lemonadeo
+//
+//  Created by elif oskanbaş on 15.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LemonadeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
